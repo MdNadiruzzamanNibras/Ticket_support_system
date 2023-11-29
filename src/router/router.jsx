17ba8c4c6a09f5d4d65e574fbb59ddd3f298d61d
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-11-29 13:06:11 +0600

    login

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../Home/Home";
 import Registration from "../Login/Registration";
+import Login from "../Login/Login";
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ export const router = createBrowserRouter([
       {
         path:"/registration",
         element:<Registration/>
+      },
+      {
+        path:"/login",
+        element:<Login/>
       },
       
       
